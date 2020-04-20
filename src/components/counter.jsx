765cac7d6e9d6ed2c18406604e353e20cf4f54b1
hannamollergren <56026748+hannamollergren@users.noreply.github.com>
2020-04-20 11:51:18 +0200

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,18 +4,8 @@ class Counter extends Component {
   state = {
 		value: this.props.value,
   };
-  
-  render() {  
-	  console.log(this.props);
-	  
-    return (
-      <div>
-	<span className={ this.getBadgeClasses() }>{ this.formatvalue() }</span>
-        <button onClick={ () => this.handleIncrement() } className="btn btn-secondary btn-sm">Increment</button><button onClick={ () => this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button> 
-      </div>
-    );
-  }
-  	getBadgeClasses(){
+
+	getBadgeClasses(){
 		let classes = "badge m-2 badge-";
 		classes += this.state.value === 0 ? 'warning' : 'primary';
 		return classes;
@@ -27,6 +17,18 @@ class Counter extends Component {
 	handleIncrement = () => {				
 		this.setState({ value: this.state.value + 1 });
 	}
+  
+  render() {  
+	  console.log(this.props);
+	  
+    return (
+      <div>
+	<span className={ this.getBadgeClasses() }>{ this.formatvalue() }</span>
+        <button onClick={ () => this.handleIncrement() } className="btn btn-secondary btn-sm">Increment</button><button onClick={ () => this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button> 
+      </div>
+    );
+  }
+  	
 }
 
 export default Counter;
